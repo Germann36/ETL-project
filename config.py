f7f40ac2f7843a480f1ae9cf47f201aba778c3ea
Germann36 <bunhammet@gmail.com>
2024-11-01 21:16:49 +0300

    Create config.py
    Добавляем файл с параметрами подключения к БД и источнику данных csv.

---
+++ config.py
@@ -0,0 +1,9 @@
+#Параметры для подключения
+param_database = "****"
+param_user = "****"
+param_password = "****"
+param_host = "****"
+param_port = "****"
+
+#Источник данных csv
+source_csv = 'C:\\Users\\Data\\source_new.csv'
